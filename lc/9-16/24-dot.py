ba8238f9ccb35a2407fbff1ec22663d967a0eac0
OmniStormX <234576150+OmniStormX@users.noreply.github.com>
2026-09-16 22:33:31 +0800

    update: 求二十四点解法的玩具。

--- lc/9-16/24-dot.py
+++ lc/9-16/24-dot.py
@@ -26,3 +26,46 @@ class Solution:
             if abs(x - 24) < 0.001:
                 return True
         return False
+
+
+# 24 点并输出答案
+
+
+# class Solution:
+#     def judgePoint24(self, cards: list[int]) -> bool:
+#         m = [[] for i in range(16)]
+#         s = [[] for i in range(16)]
+#         for i in range(4):
+#             m[(1 << i)].append(cards[i])
+#             s[(1 << i)].append(str(cards[i]))
+#         for i in range(16):
+#             if i == i & -i:
+#                 continue
+#             j = i & (i - 1)
+#             while j > 0:
+#                 for i1, x in enumerate(m[i ^ j]):
+#                     for i2, y in enumerate(m[j]):
+#                         s1 = "(" + s[i ^ j][i1] + ")"
+#                         s2 = "(" + s[j][i2] + ")"
+
+#                         m[i].append(x + y)
+#                         s[i].append(s1 + "+" + s2)
+#                         m[i].append(x - y)
+#                         s[i].append(s1 + "-" + s2)
+#                         m[i].append(y - x)
+#                         s[i].append(s2 + "-" + s1)
+#                         m[i].append(x * y)
+#                         s[i].append(s1 + "*" + s2)
+#                         if y != 0:
+#                             m[i].append(x / y)
+#                             s[i].append(s1 + "/" + s2)
+#                         if x != 0:
+#                             m[i].append(y / x)
+#                             s[i].append(s2 + "/" + s1)
+#                 j = i & (j - 1)
+#         # print(m[15])
+#         for i, x in enumerate(m[15]):
+#             if abs(x - 24) < 0.001:
+#                 print(s[15][i])
+#                 return True
+#         return False
